e7daa415ce0fa1ef224688b007971303b41c5471
DenverW03 <d.whiteley@lancaster.ac.uk>
2024-04-14 16:41:44 +0100

    file upload and saving!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::fs::File;
+use std::io::Error;
+use std::io::Write;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,10 +10,25 @@ async fn hello() -> impl Responder {
 
 #[post("/post")]
 async fn upload(body: web::Bytes) -> impl Responder {
-    // TODO: Implement file upload logic
     println!("File RECEIVED!");
 
+    // Saving the file to local storage
+    let file_bytes = body.clone();
+    let file_path: String =
+        "/Users/denver/Documents/Personal/syncer/platform-syncer/files/this.txt".to_string();
+    if let Err(err) = save_file(file_bytes, file_path).await {
+        println!("Error saving file: {}", err);
+    } else {
+        println!("File saved successfully");
+    }
+
     HttpResponse::Ok().body(format!("Received {} bytes", body.len()))
+}
+
+async fn save_file(file_bytes: web::Bytes, file_path: String) -> Result<(), Error> {
+    let mut file = File::create(file_path)?;
+    file.write_all(&file_bytes)?;
+    Ok(())
 }
 
 #[actix_web::main]
