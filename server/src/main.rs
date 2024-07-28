b378efc36247903e110af2f41955abbb6dfd08e4
DenverW03 <d.whiteley@lancaster.ac.uk>
2024-07-28 14:06:55 +0100

    lmao i included the http in the url serverside

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,8 @@
+use std::fs;
+
 use actix_web::{App, HttpServer};
 use database_interface::check_database;
+use tokio::fs::File;
 
 mod database_interface;
 mod rest_api;
@@ -16,13 +19,22 @@ async fn main() -> std::io::Result<()> {
         Err(_) => println!("Failed to setup the database"),
     }
 
+    // Loading the URL from the "./url.txt" file
+    let url = match fs::read_to_string("./url.txt") {
+        Ok(url) => url.replace("\n", ""),
+        Err(_) => {
+            println!("Failed to read file string");
+            return Ok(());
+        }
+    };
+
     HttpServer::new(|| {
         App::new()
             .service(rest_api::upload)
             .service(rest_api::last_modified)
             .service(rest_api::get_sync)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(url)?
     .run()
     .await
 }
